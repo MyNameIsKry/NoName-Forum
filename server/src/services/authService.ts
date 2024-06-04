7eba4c31a0ace1a513bfa745e431e0b7f096cf2f
KrySoSad <duythuc070205@gmail.com>
2024-06-04 21:10:13 +0700

    ✅ feat: Add refresh token and logout services

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -1,7 +1,8 @@
 import { hash, genSalt, compare } from 'bcrypt';
 import { prisma } from '..';
-import jwt from "jsonwebtoken";
 import envConfig from '../config';
+import { GenerateToken } from '../utils/generateToken';
+import jwt from "jsonwebtoken";
 //import { createTransport } from "nodemailer";
 
 export interface RegisterRequestBody {
@@ -21,9 +22,14 @@ export class AuthService {
     public static async register(data: RegisterRequestBody) {
         const { username, password, email } = data;
 
-        const existingUser = await prisma.user.findUnique({ where: { email } });
-        if (existingUser)
+        const existingUserEmail = await prisma.user.findUnique({ where: { email } });
+        const existingUsername = await prisma.user.findUnique({ where: { username } })
+
+        if (existingUserEmail)
             return { error: "Email đã tồn tại" };
+
+        if (existingUsername)
+            return { error: "Username đã tồn tại" };
 
         const salt = await genSalt(10);
         const hashedPassword = await hash(password, salt);
@@ -31,9 +37,11 @@ export class AuthService {
         const user = await prisma.user.create({
             data: {
                 username: username,
+                display_name: username,
                 password: hashedPassword,
                 email: email,
-                registered_at: new Date()
+                registered_at: new Date(),
+                updated_at: new Date(),
             }
         });
         
@@ -41,21 +49,66 @@ export class AuthService {
     }
 
     public static async login(data: LoginRequestBody) {
-        const { email, password } = data;
+        try {
+            const { email, password } = data;
 
-        const user = await prisma.user.findUnique({ where: { email } });
+            const user = await prisma.user.findUnique({ where: { email } });
+    
+            if (!user || !(await compare(password, user.password))) {
+                return { error: "Email hoặc password sai!" };
+            }
+    
+            const accessToken = new GenerateToken("access_token", envConfig?.JWT_ACCESS_TOKEN_EXPIRES_IN as string).generate(user);
+            const refreshToken = new GenerateToken("refresh_token", envConfig?.JWT_REFRESH_TOKEN_EXPIRES_IN as string).generate(user);
 
-        if (!user || !(await compare(password, user.password))) {
-            return { error: "Email hoặc password sai!" };
+            await prisma.user.update({
+                where: { id: user.id },
+                data: { refresh_token: refreshToken }
+            })
+
+            return { user, accessToken, refreshToken };   
+        } catch (err) {
+            console.log(err);
+            return;
         }
+    }
 
-        const token = jwt.sign(
-            { userId: user.id, role: user.role },
-            envConfig?.SESSION_TOKEN_SECRET as string,
-        );
+    public static async refreshToken(token: string) {
+        try {
+            const payload = jwt.verify(token, envConfig?.JWT_SECRET as string) as { userId: string, role: string }; 
+            const user = await prisma.user.findUnique({ where: { id: payload.userId } });
 
-        console.log(token);
+            if (!user)
+                return { error: "Invalid Token!" };
 
-        return { user, token };
+            const accessToken = new GenerateToken("access_token", envConfig?.JWT_ACCESS_TOKEN_EXPIRES_IN as string).generate(user);
+            const refreshToken = new GenerateToken("refresh_token", envConfig?.JWT_REFRESH_TOKEN_EXPIRES_IN as string).generate(user);
+
+            await prisma.user.update({
+                where: { id: user.id },
+                data: { refresh_token: refreshToken }
+            });
+
+            return { accessToken, refreshToken };   
+            
+        } catch(err) {
+            console.log(err);
+            return {error: err};
+        }
+    }
+
+    public static async logout(refreshToken: string) {
+        try {
+            const payload = jwt.verify(refreshToken, envConfig?.JWT_SECRET as string) as { userId: string, role: string }; 
+            const user = await prisma.user.findUnique({ where: { id: payload.userId } });
+
+            await prisma.user.update({
+                where: { id: user?.id },
+                data: { refresh_token: null }
+            })
+            
+        } catch(err) {
+            console.log(err);
+        }
     }
 }
--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -3,8 +3,21 @@ import { prisma } from "..";
 export class UserService {
     constructor() {}
 
-    public static async getUserInfo(email: string) {
-        const user = await prisma.user.findUnique({ where: { email } });
+    public static async getUserInfo(username: string) {
+        const user = await prisma.user.findUnique({ 
+            where: { username },
+            select: {
+                username: true,
+                display_name: true,
+                bio: true,
+                avatar_url: true,
+                role: true,
+                posts: true,
+                registered_at: true,
+            }
+        });
+        if (!user) 
+            return { error: "Không tìm thấy username này!" };
         return user;
     }
 }
