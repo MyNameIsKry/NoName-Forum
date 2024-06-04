d59ef61ee40a14b2b4874159907c807c753a5b80
KrySoSad <duythuc070205@gmail.com>
2024-06-04 21:07:22 +0700

    ✅ feat: Add refresh token route and logout route

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -4,4 +4,6 @@ import { AuthController } from '../controllers/authController';
 export async function authRoutes(fastify: FastifyInstance) {
     fastify.post('/register', AuthController.register);
     fastify.post('/login', AuthController.login);
+    fastify.post('/refresh', AuthController.refreshToken);
+    fastify.post('/logout', AuthController.logout);
 }
--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { UserController } from '../controllers/userController';
+import authMiddleware from '../middlewares/authMiddleware';
 
 export async function userRoutes(fastify: FastifyInstance) {
+    await authMiddleware(fastify);
+
     fastify.get("/search", UserController.getUserInfo);
 }
