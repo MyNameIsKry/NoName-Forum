6a1aa18fb147904f0728040d31f49e584a3bf365
KrySoSad <duythuc070205@gmail.com>
2024-06-09 09:50:58 +0700

    ✅ feat: Add swagger docs for api

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,37 @@ import { postRoutes } from './routes/postRoute'
 import cookie from "@fastify/cookie"
 import type { FastifyCookieOptions } from '@fastify/cookie'
 import envConfig from './config'
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const fastify = Fastify({
   logger: true
 })
+
+const swaggerOptions = {
+  swagger: {
+      info: {
+          title: "NoName Forum",
+          description: "Documents.",
+          version: "1.0.0",
+      },
+      host: "localhost",
+      schemes: ["http", "https"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      tags: [{ name: "Default", description: "Default" }],
+  },
+};
+
+const swaggerUiOptions = {
+  routePrefix: "/docs",
+  exposeRoute: true,
+};
+
+fastify.register(fastifySwagger, swaggerOptions);
+fastify.register(fastifySwaggerUi, swaggerUiOptions);
+
+
 
 fastify.register(cookie, {
   secret: envConfig?.COOKIE_SECRET as string,
@@ -21,14 +48,15 @@ fastify.get('/', (request, reply) => {
 })
 
 fastify.register(authRoutes, { prefix: "/auth" });
-fastify.register(userRoutes, { prefix: "/user" });
-fastify.register(postRoutes, { prefix: "/post" });
+fastify.register(userRoutes, { prefix: "/users" });
+fastify.register(postRoutes, { prefix: "/posts" });
+
 
 const start = async () => {
-    fastify.listen({ port: 3300 }, (err, address) => {
-      if (err) throw err;
-    });
-    await PrismaConnect.connect();
+  fastify.listen({ port: 3300 }, (err, address) => {
+    if (err) throw err;
+  });
+  await PrismaConnect.connect();
 }
 
 const prisma = PrismaConnect.getPrismaClient();
