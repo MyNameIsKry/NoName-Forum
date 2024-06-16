20601e47f438d863c8ba37cbd113f58581b034db
KrySoSad <duythuc070205@gmail.com>
2024-06-16 19:30:34 +0700

    ♻️ refactor: rename api add get post by id

--- server/src/routes/public/postRoute.ts
+++ server/src/routes/public/postRoute.ts
@@ -4,8 +4,10 @@ import authMiddleware from '../../middlewares/authMiddleware';
 
 export async function postRoutes(fastify: FastifyInstance) {
     await authMiddleware(fastify);
-    fastify.get("/all", PostController.getAllPost);
-    fastify.post("/new", PostController.createNewPost);
-    fastify.get("/search", PostController.getPostByName);
-    fastify.get("/category/:categoryName", PostController.getPostByCategory);
+    fastify.get("/", PostController.getAllPost); // Lấy tất cả bài viết
+    fastify.get("/:postId", PostController.getPostById); // Lấy bài viết theo ID
+    fastify.get("/search", PostController.getPostByName); // Tìm kiếm bài viết theo tên
+    fastify.get("/category/:categoryName", PostController.getPostByCategory); // Lấy bài viết theo thể loại
+    
+    fastify.post("/", PostController.createNewPost); // Tạo bài viết mới
 }
