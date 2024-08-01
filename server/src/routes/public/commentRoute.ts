6bf015babfb833bb3c754465298ce22f377a2e60
KrySoSad <duythuc070205@gmail.com>
2024-08-01 22:57:19 +0700

    ✅ feat: add post comment route

--- server/src/routes/public/commentRoute.ts
+++ server/src/routes/public/commentRoute.ts
@@ -4,5 +4,6 @@ import authMiddleware from '../../middlewares/authMiddleware';
 
 export async function commentRoutes(fastify: FastifyInstance) {
     await authMiddleware(fastify);
-    fastify.post("/:postId/comments", CommentController.createComment); // Tạo bình luận mới cho bài viết cụ thể
+    fastify.post("/:postId/comments", { preValidation: [fastify.authMiddleware] }, CommentController.createComment); // Tạo bình luận mới cho bài viết cụ thể
+    fastify.get("/:postId/comments", { preValidation: [fastify.authMiddleware] }, CommentController.getAllComments);
 }
