2f2c00a07fc3dc8a5e89fc82d51354d79701ffa2
KrySoSad <duythuc070205@gmail.com>
2024-08-09 12:48:32 +0700

    login with google

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,14 +4,50 @@ import { Sidebar } from "@/components/Sidebar";
 import { Inter } from 'next/font/google'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 library.add(fas);
 
-export default function Home() {
+interface IUserInfo {
+  username: string;
+  display_name: string;
+  bio: string | null;
+  avatar_url: string | null;
+  role: string;
+  registered_at: Date;
+}
+
+const Home: React.FC = () => {
+  const [userData, setUserData] = useState<IUserInfo | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const cookie = document.cookie;
+        const token = cookie.split('accessToken=')[1];
+
+        if (token) {
+          const response = await axios.get<IUserInfo>(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          console.log("Data: ", response.data);
+          setUserData(response.data);
+        } else console.log("No data")
+      } catch (err) {
+        console.error("Error get userData: ", err);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <main className={`bg-gray-900 bg w-dvw h-dvh ${inter.className} flex-1 p-4`}>
-      <Header/>
+      <Header userData={userData}/>
       <div className="flex flex-1">
         <Sidebar/>
 
@@ -19,3 +55,5 @@ export default function Home() {
     </main>
   );
 }
+
+export default Home
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,8 +2,22 @@ import Link from 'next/link';
 import { faBell, faCaretDown, faMessage, faHouse } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const Header = () => {
+interface IUserInfo {
+  username: string;
+  display_name: string;
+  bio: string | null;
+  avatar_url: string | null;
+  role: string;
+  registered_at: Date;
+}
 
+interface HeaderProps {
+  userData: IUserInfo | null;
+}
+
+const Header: React.FC<HeaderProps> = ({ userData }) => {
+  const imgUrl = userData?.avatar_url;
+  
   return (
     <header className="bg-gray-800 text-white py-4 px-10 mb-5 rounded-lg">
       <div className="container mx-auto flex justify-between items-center">
@@ -12,19 +26,22 @@ const Header = () => {
             <h1 className='font-bold text-3xl'>LOGO</h1>
           </Link> 
         </div>
-        <div className='flex items-center gap-9 font-bold'>
-          <Link href="/login">
-            Đăng nhập
-          </Link>
-          <Link 
-            href="/register" 
-            className='bg-purple-600 hover:bg-opacity-80 transition duration-200 py-3 px-6 rounded-md'  
-          >
-            Đăng kí
-          </Link>
-        </div>
         {
-          false &&
+          !userData &&
+          <div className='flex items-center gap-9 font-bold'>
+            <Link href="/login">
+              Đăng nhập
+            </Link>
+            <Link 
+              href="/register" 
+              className='bg-purple-600 hover:bg-opacity-80 transition duration-200 py-3 px-6 rounded-md'  
+            >
+              Đăng kí
+            </Link>
+          </div>
+        }
+        {
+          userData &&
           <>
             <div className='flex items-center gap-6 max-lg:hidden'>
               <div className='bg-input-color p-4 rounded-lg'>
@@ -35,15 +52,14 @@ const Header = () => {
               </div>
               <div className='flex items-center bg-input-color p-2 rounded-lg gap-3 cursor-pointer'>
                 <div className='w-10 h-10 border rounded-3xl'>
-                  <img className="rounded-3xl" src="https://images-ext-1.discordapp.net/external/9YypUckVSLonoKmFV5L2Sn9xLV00Bk_qn2xrgzw6v0Q/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/282859044593598464/156a0d2872579f1ffcaa5d2127239bfd.png?format=webp&quality=lossless&width=702&height=702" alt="user avatar" />
+                  <img className="rounded-3xl" src={`${userData.avatar_url}`} alt="user avatar" />
                 </div>
-                <h1 className='font-bold text-xl'>Name</h1>
+                <h1 className='font-bold text-xl'>{userData.display_name}</h1>
                 <FontAwesomeIcon icon={faCaretDown} className='h-5 w-5' />  
               </div>
             </div>
           </> 
         }
-        {/* font xấu vcl */}
       </div>
     </header>
   );
