1b0422c858fb2cb173792c807971f78eb77e2c19
KrySoSad <duythuc070205@gmail.com>
2024-10-13 21:02:57 +0700

    ♻️ refactor: include votes in post query

--- server/src/services/commentService.ts
+++ server/src/services/commentService.ts
@@ -4,16 +4,27 @@ import { IPost, IComment } from "./postService";
 
 const cache = createNodeCache(300);
 
+interface IPostHasId {
+    author_name: string;
+    title: string;
+    content: string;
+    comments: IComment[];
+    created_at: Date;
+    updated_at: Date;
+    category_name: string;
+}
+
 export class CommentService {
     constructor() {}
 
     // Lấy all comments của bài viết cụ thể
     public static async getAllComments(postId: string): Promise<IComment[] | { error: string }> { 
         try {
-            const cachePostId = cache.get<IPost>(`post_id:${postId}`);
+            const cachePostId = cache.get<IPostHasId>(`post_id:${postId}`);
 
-            if (cachePostId && cachePostId.comments)
-                return cachePostId.comments;
+            if (cachePostId && cachePostId.comments) {
+                return cachePostId.comments; 
+            }
 
             const comments = await prisma.comment.findMany({
                 where: { post_id: postId, parent_id: null },
@@ -31,21 +42,29 @@ export class CommentService {
                     created_at: true,
                     updated_at: true,
                 }
-            })
+            });
 
             if (cachePostId) {
                 cachePostId.comments = comments;
-                cache.set(`post_id:${postId}`, cachePostId);
-            } else  {}
-                cache.set(`post_id:${postId}`, { id: postId, comments: comments})
+                cache.set(`post_id:${postId}`, cachePostId); 
+            } else {
+                cache.set(`post_id:${postId}`, {
+                    author_name: "",
+                    title: "",
+                    content: "",
+                    comments: comments,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                    category_name: "",
+                });
+            }
 
-            console.log(cachePostId);
             return comments;
         } catch (err) {
             console.log(err);
-            return { error: err instanceof Error ? err.message : "Unknow error occured"};
+            return { error: err instanceof Error ? err.message : "Unknown error occurred" };
         }
-    } 
+    }
 
     public static async createComment(postId: string, authorId: string, authorName: string, content: string) {
         try {   
@@ -56,18 +75,18 @@ export class CommentService {
                     author_id: authorId,
                     content: content,
                 }
-            })
+            });
 
             const cachePost = cache.get<IPost>(`post_id:${postId}`);
             if (cachePost) {
                 delete (cachePost as any).comments;
                 cache.set(`post_id:${postId}`, cachePost);
             }            
-            return newComment;
 
+            return newComment;
         } catch (err) {
             console.log(err);            
-            return { error: err instanceof Error ? err.message : "Unknow error occured"};
+            return { error: err instanceof Error ? err.message : "Unknown error occurred" };
         }
     }
 }
