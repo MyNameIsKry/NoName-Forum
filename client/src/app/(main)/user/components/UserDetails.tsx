925e6f877800afe9ddc5d8e6f1185043ee18d8d8
KrySoSad <duythuc070205@gmail.com>
2024-12-15 19:22:50 +0700

    chore: add details information of user

---
+++ client/src/app/(main)/user/components/UserDetails.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from 'react'
+import { Box, Typography, Paper } from "@mui/material";
+
+type UserDetailsProps = {
+    nickname: string;
+    role: string;
+    post: number;
+    registeredDay: Date;
+}
+
+const UserDetails = ({ nickname, role, post, registeredDay }: UserDetailsProps) => {
+  return (
+    <Paper className="bg-gray-800 text-white p-4 rounded-lg h-full flex items-center">
+        <Box>
+            <Typography variant="body1">Nickname: {nickname}</Typography>
+            <Typography variant="body1">Vai trò: {role}</Typography>
+            <Typography variant="body1">Số bài viết đã đăng: {post}</Typography>
+            <Typography variant="body1">Số điểm: 0</Typography>
+            <Typography variant="body1">
+            Ngày tạo tài khoản: {new Date(registeredDay).toLocaleDateString("vi-VN")}
+            </Typography>
+        </Box>
+    </Paper>
+  )
+}
+
+export default UserDetails;
