6da73c015d2dd6593ce84fd60bb104c0a483abfc
KrySoSad <duythuc070205@gmail.com>
2024-12-15 19:23:15 +0700

    feat: add props for custom button

--- client/src/components/Button.ts
+++ client/src/components/Button.ts
@@ -2,13 +2,27 @@ import Button, { ButtonProps } from "@mui/material/Button";
 import { styled } from "@mui/material";
 import { purple } from "@mui/material/colors";
 
-export const CustomButton = styled(Button)<ButtonProps>(({ theme }) => ({
+interface CustomButtonProps extends ButtonProps {
+  backgroundcolor?: string;
+  hoverbackgroundcolor?: string;
+  gap?: string;
+  padding?: string;
+}
+
+export const CustomButton = styled(Button)<CustomButtonProps>((
+  { 
+    theme, 
+    gap = "10px", 
+    padding = "12px 24px", 
+    backgroundcolor = purple[600],
+    hoverbackgroundcolor = purple[700]
+  }) => ({
     color: theme.palette.getContrastText(purple[500]),
-    padding: "12px 24px",
+    padding: padding,
     fontWeight: "bold",
-    backgroundColor: purple[600],
-    gap: "10px",
+    backgroundColor: backgroundcolor,
+    gap: gap,
     '&:hover': {
-      backgroundColor: purple[700],
+      backgroundColor: hoverbackgroundcolor,
     },
 }));
